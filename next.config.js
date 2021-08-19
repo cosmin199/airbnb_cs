ed4ef037f7dc83a4eb3542b13bd41d44b8793200
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-08-19 00:02:49 -0700

    finish the map

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1Ijoic2ViYTcxMSIsImEiOiJja3NpZ2Q0MjUwcnluMnZtYm50bjRxM2t4In0.4Y93ONgV5Amdw6hsP28W3A",
+  },
 }
